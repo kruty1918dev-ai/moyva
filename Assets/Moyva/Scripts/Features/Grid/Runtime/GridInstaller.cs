adc18174a7370887cb70a526c4c37583874acd03
Kruty1918 <kruty1918.dev@gmail.com>
2026-03-26 16:32:34 +0100

    Added grid

---
+++ Assets/Moyva/Scripts/Features/Grid/Runtime/GridInstaller.cs
@@ -0,0 +1,19 @@
+using Kruty1918.Moyva.Grid.API;
+using UnityEngine;
+using Zenject;
+
+namespace Kruty1918.Moyva.Grid
+{
+    public class GridInstaller : MonoInstaller
+    {
+       [SerializeField] private int gridWidth = 10;
+        [SerializeField] private int gridHeight = 10;
+        [SerializeField] private float tileSize = 1f;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<IGridService>().To<GridService>().AsSingle()
+                .WithArguments(gridWidth, gridHeight, tileSize);
+        }
+    }
+}
