9b16785b50e8ec0fcc42b846c9321c973adc05d0
Kruty1918 <kruty1918.dev@gmail.com>
2026-03-26 23:05:32 +0100

    Added generation tiles

---
+++ Assets/Moyva/Scripts/Features/Generator/Runtime/GeneratorInstaller.cs
@@ -0,0 +1,21 @@
+using Kruty1918.Moyva.DTO;
+using Kruty1918.Moyva.Grid.API;
+using UnityEngine;
+using Zenject;
+
+namespace Kruty1918.Moyva.Generator
+{
+    public class GeneratorInstaller : MonoInstaller
+    {
+        [SerializeField] private TileView _tileViewPrefab;
+
+        public override void InstallBindings()
+        {
+            TileGenerator tileGenerator = new TileGenerator(_tileViewPrefab, Container.Resolve<IGridService>());
+
+            Container.Bind<TileGenerator>().FromInstance(tileGenerator).AsSingle();
+
+            tileGenerator.GenerateTiles();
+        }
+    }
+}
