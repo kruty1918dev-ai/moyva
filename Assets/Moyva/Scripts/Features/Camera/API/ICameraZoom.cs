13b83cd8100c9e084b31c47e5e0b821f219a7a65
Kruty1918 <kruty1918.dev@gmail.com>
2026-05-11 13:54:03 +0200

    chore(split): camera 01/02
    Branch split commit 012/100.
    
    Scope: camera
    Files: 4

--- Assets/Moyva/Scripts/Features/Camera/API/ICameraZoom.cs
+++ Assets/Moyva/Scripts/Features/Camera/API/ICameraZoom.cs
@@ -3,6 +3,7 @@ namespace Kruty1918.Moyva.Camera.API
     public interface ICameraZoom
     {
         void ZoomCamera(float zoomAmount);
+        void ZoomCameraByScale(float scaleFactor, bool immediate);
         void ForceZoomCamera(float zoomLevel);
     }
 }
