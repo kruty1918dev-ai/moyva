4fe41a7cd18daf8365fef08b2fd6fa3c02f4eb7a
Moyva PatchKit <patchkit@localhost>
2026-08-12 11:05:58 +0200

    patchkit/repair-player-json-factory-references: apply-unified-diff

--- Assets/Moyva/Scripts/Features/Camera/Runtime/CameraInstaller.cs
+++ Assets/Moyva/Scripts/Features/Camera/Runtime/CameraInstaller.cs
@@ -2,6 +2,7 @@ using System;
 using Kruty1918.Moyva.Camera.API;
 using Kruty1918.Moyva.Grid.API;
 using Kruty1918.Moyva.InputRouting.Runtime;
+using Kruty1918.Moyva.Jsonization;
 using Kruty1918.Moyva.MapChunks.Runtime;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,7 +10,6 @@ using Zenject;
 
 #if UNITY_EDITOR
 using UnityEditor;
-using Kruty1918.Moyva.Jsonization;
 #endif
 
 namespace Kruty1918.Moyva.Camera.Runtime
