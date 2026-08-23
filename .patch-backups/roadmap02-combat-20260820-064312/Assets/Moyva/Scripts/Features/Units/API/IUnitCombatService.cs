4a71c22186cdefb1d7df45b8acf04e8862733f1f
Kruty1918 <kruty1918.dev@gmail.com>
2026-08-23 21:51:02 +0200

    update

--- .patch-backups/roadmap02-combat-20260820-064312/Assets/Moyva/Scripts/Features/Units/API/IUnitCombatService.cs
+++
@@ -1,8 +0,0 @@
-namespace Kruty1918.Moyva.Units.API
-{
-    public interface IUnitCombatService
-    {
-        bool TryPreviewAttack(string attackerUnitId, string defenderUnitId, out UnitCombatBreakdown breakdown);
-        bool TryPreviewDuel(string attackerUnitId, string defenderUnitId, out UnitCombatDuel duel);
-    }
-}
