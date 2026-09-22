644657b56c4d5efe2f64c445c6d754b2900903a2
Oleksiy Havryliuk <kruty1918.dev@gmail.com>
2026-09-22 08:58:57 +0200

    Update GameplayStartupFirstFrameTests.cs

--- Assets/Moyva/Scripts/Features/HomeMenu/Tests/Editor/GameplayStartupFirstFrameTests.cs
+++ Assets/Moyva/Scripts/Features/HomeMenu/Tests/Editor/GameplayStartupFirstFrameTests.cs
@@ -31,7 +31,12 @@ namespace Kruty1918.Moyva.Tests.HomeMenu
             var graphics = new RecordingGraphics(order);
             var prewarm = new RecordingPrewarm(order);
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             var config = new HomeMenuConfigSO { gameplaySceneName = "__p082_missing_scene__" };
+=======
+            var config = ScriptableObject.CreateInstance<HomeMenuConfigSO>();
+            config.gameplaySceneName = "__p082_missing_scene__";
+>>>>>>> Stashed changes
 =======
             var config = ScriptableObject.CreateInstance<HomeMenuConfigSO>();
             config.gameplaySceneName = "__p082_missing_scene__";
